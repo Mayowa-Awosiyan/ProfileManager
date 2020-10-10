c23e4268a486bdd8d423dcd3ec302299b9c935b9
Mayowa-Awosiyan <oawos064@uottawa.ca>
2020-10-09 23:03:03 -0400

    Done all but camera

--- app/src/main/java/com/example/profilemanager/ImagesList.java
+++ app/src/main/java/com/example/profilemanager/ImagesList.java
@@ -3,9 +3,15 @@ package com.example.profilemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.util.Date;
 
 public class ImagesList extends AppCompatActivity {
 
@@ -26,4 +32,5 @@ public class ImagesList extends AppCompatActivity {
         //Finishing Activity and return to main screen!
         finish();
     }
+    
 }
